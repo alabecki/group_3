f9299cf5a3cd5974950e2196bf1fe23a60a7e58c
alabecki <alabecki@sfu.ca>
2016-02-17 07:27:40 +0000

    Finish user signup

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,28 @@
+class UsersController < ApplicationController
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def new
+    @user = User.new
+  end
+
+ def create
+    @user = User.new(user_params)
+    if @user.save
+      # flash[:success] = "Welcome to the Random Encounters!"   <will need views/layouts/application.html.erb to work>
+      redirect_to @user
+    else
+      render 'new'
+    end
+ end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:username, :email, :password, :street_address, :city, :country, :postal_code,
+                                   :password_confirmation)
+    end
+end
+
