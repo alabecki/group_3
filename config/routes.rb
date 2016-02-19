864dbed2a1474a1941c7fbe65d0010ddac283061
Lily Doo <ldoo@sfu.ca>
2016-02-18 21:27:48 -0800

    google maps api page setup

--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,10 @@ Rails.application.routes.draw do
   get    'login'   => 'sessions#new'
   post   'login'   => 'sessions#create'
   delete 'logout'  => 'sessions#destroy'
-
-
+  get 'maps' => 'map#index'
   get 'signup'  => 'users#new'
   resources :users
-
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
