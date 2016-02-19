ad26371dcfcee08e4256c5e6adf335d2399b634d
alabecki <alabecki@sfu.ca>
2016-02-19 05:56:52 +0000

    drop down menu fix

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
 
+  before_action :logged_in_user, only: [:edit, :update]
   def show
     @user = User.find(params[:id])
   end
@@ -8,16 +9,30 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
- def create
+  def create
     @user = User.new(user_params)
     if @user.save
-       log_in @user
-      # flash[:success] = "Welcome to the Random Encounters!"   <will need views/layouts/application.html.erb to work>
+      log_in @user
+      #flash[:success] = "Welcome to the Sample App!"
       redirect_to @user
     else
       render 'new'
     end
- end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+ def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      #flash[:success] = "Profile updated"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
 
   private
 
