fcf641af58f487bcf0e840c5a5651a2987c0f9bf
alabecki <alabecki@sfu.ca>
2016-03-05 21:39:42 +0000

    double check

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User<ActiveRecord::Base
+  
+  attr_accessor :remember_token
   before_save { self.email = email.downcase }
   validates :username,  presence: true, length: { maximum: 50 }
   validates :street_address,  presence: true, length: { maximum: 100 }
@@ -22,6 +24,24 @@ class User<ActiveRecord::Base
     
   def address
     [street_address, city, country].join(' ')
+  end
+  
+   def User.new_token
+    SecureRandom.urlsafe_base64
+   end
+   
+    def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
+    end
+    
+     def authenticated?(remember_token)
+        return false if remember_digest.nil?
+        BCrypt::Password.new(remember_digest).is_password?(remember_token)
+     end
+     
+  def forget
+    update_attribute(:remember_digest, nil)
   end
   
   geocoded_by :address
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160228233251) do
+ActiveRecord::Schema.define(version: 20160229002852) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(version: 20160228233251) do
     t.string   "password_digest"
     t.text     "description"
     t.boolean  "admin"
+    t.string   "remember_digest"
   end
 
 end
