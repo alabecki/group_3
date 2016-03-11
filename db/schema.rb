77141a2339b45b715a8d8e2d4b7d0ddf6d99d5b9
Lily Doo <ldoo@sfu.ca>
2016-03-10 17:37:25 -0800

    gem heroku

--- db/schema.rb
+++ db/schema.rb
@@ -16,30 +16,130 @@ ActiveRecord::Schema.define(version: 20160309080812) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "forem_categories", force: :cascade do |t|
+    t.string   "name",       limit: 255,             null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "slug",       limit: 255
+    t.integer  "position",               default: 0
+  end
+
+  add_index "forem_categories", ["slug"], name: "index_forem_categories_on_slug", unique: true, using: :btree
+
+  create_table "forem_forums", force: :cascade do |t|
+    t.string  "name",        limit: 255
+    t.text    "description"
+    t.integer "category_id"
+    t.integer "views_count",             default: 0
+    t.string  "slug",        limit: 255
+    t.integer "position",                default: 0
+  end
+
+  add_index "forem_forums", ["slug"], name: "index_forem_forums_on_slug", unique: true, using: :btree
+
+  create_table "forem_groups", force: :cascade do |t|
+    t.string "name", limit: 255
+  end
+
+  add_index "forem_groups", ["name"], name: "index_forem_groups_on_name", using: :btree
+
+  create_table "forem_memberships", force: :cascade do |t|
+    t.integer "group_id"
+    t.integer "member_id"
+  end
+
+  add_index "forem_memberships", ["group_id"], name: "index_forem_memberships_on_group_id", using: :btree
+
+  create_table "forem_moderator_groups", force: :cascade do |t|
+    t.integer "forum_id"
+    t.integer "group_id"
+  end
+
+  add_index "forem_moderator_groups", ["forum_id"], name: "index_forem_moderator_groups_on_forum_id", using: :btree
+
+  create_table "forem_posts", force: :cascade do |t|
+    t.integer  "topic_id"
+    t.text     "text"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.integer  "reply_to_id"
+    t.string   "state",       limit: 255, default: "pending_review"
+    t.boolean  "notified",                default: false
+  end
+
+  add_index "forem_posts", ["reply_to_id"], name: "index_forem_posts_on_reply_to_id", using: :btree
+  add_index "forem_posts", ["state"], name: "index_forem_posts_on_state", using: :btree
+  add_index "forem_posts", ["topic_id"], name: "index_forem_posts_on_topic_id", using: :btree
+  add_index "forem_posts", ["user_id"], name: "index_forem_posts_on_user_id", using: :btree
+
+  create_table "forem_subscriptions", force: :cascade do |t|
+    t.integer "subscriber_id"
+    t.integer "topic_id"
+  end
+
+  create_table "forem_topics", force: :cascade do |t|
+    t.integer  "forum_id"
+    t.integer  "user_id"
+    t.string   "subject",      limit: 255
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.boolean  "locked",                   default: false,            null: false
+    t.boolean  "pinned",                   default: false
+    t.boolean  "hidden",                   default: false
+    t.datetime "last_post_at"
+    t.string   "state",        limit: 255, default: "pending_review"
+    t.integer  "views_count",              default: 0
+    t.string   "slug",         limit: 255
+  end
+
+  add_index "forem_topics", ["forum_id"], name: "index_forem_topics_on_forum_id", using: :btree
+  add_index "forem_topics", ["slug"], name: "index_forem_topics_on_slug", unique: true, using: :btree
+  add_index "forem_topics", ["state"], name: "index_forem_topics_on_state", using: :btree
+  add_index "forem_topics", ["user_id"], name: "index_forem_topics_on_user_id", using: :btree
+
+  create_table "forem_views", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "viewable_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.integer  "count",                         default: 0
+    t.string   "viewable_type",     limit: 255
+    t.datetime "current_viewed_at"
+    t.datetime "past_viewed_at"
+  end
+
+  add_index "forem_views", ["updated_at"], name: "index_forem_views_on_updated_at", using: :btree
+  add_index "forem_views", ["user_id"], name: "index_forem_views_on_user_id", using: :btree
+  add_index "forem_views", ["viewable_id"], name: "index_forem_views_on_viewable_id", using: :btree
+
   create_table "users", force: :cascade do |t|
-    t.string   "username"
-    t.string   "email"
-    t.string   "street_address"
-    t.string   "city"
-    t.string   "country"
-    t.string   "postal_code"
+    t.string   "username",             limit: 255
+    t.string   "email",                limit: 255
+    t.string   "street_address",       limit: 255
+    t.string   "city",                 limit: 255
+    t.string   "country",              limit: 255
+    t.string   "postal_code",          limit: 255
     t.boolean  "lol"
     t.boolean  "dota2"
     t.boolean  "smite"
     t.boolean  "hots"
-    t.datetime "created_at",                        null: false
-    t.datetime "updated_at",                        null: false
+    t.datetime "created_at",                                                  null: false
+    t.datetime "updated_at",                                                  null: false
     t.float    "longitude"
     t.float    "latitude"
-    t.string   "password_digest"
+    t.string   "password_digest",      limit: 255
     t.text     "description"
-    t.boolean  "admin",             default: false
-    t.string   "remember_digest"
-    t.string   "activation_digest"
-    t.boolean  "activated",         default: false
+    t.boolean  "admin",                            default: false
+    t.string   "remember_digest",      limit: 255
+    t.boolean  "forem_admin",                      default: false
+    t.string   "forem_state",          limit: 255, default: "pending_review"
+    t.boolean  "forem_auto_subscribe",             default: false
+    t.string   "activation_digest",    limit: 255
+    t.boolean  "activated",                        default: false
     t.datetime "activated_at"
-    t.string   "state"
-    t.string   "reset_digest"
+    t.string   "state",                limit: 255
+    t.string   "reset_digest",         limit: 255
     t.datetime "reset_sent_at"
   end
 
