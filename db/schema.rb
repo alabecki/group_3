580f11a779b6ab0b97ac24970b163974f34fc9ac
Derek <djbunsko@gmail.com>
2016-03-12 13:23:19 -0800

    pushing twitter stuff

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160309080812) do
+ActiveRecord::Schema.define(version: 20160311000208) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,6 +41,7 @@ ActiveRecord::Schema.define(version: 20160309080812) do
     t.string   "state"
     t.string   "reset_digest"
     t.datetime "reset_sent_at"
+    t.string   "twitter_name"
   end
 
 end
