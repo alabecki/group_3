3ea85f1335e3b8d29c49e9fc6167796d44f05416
Lily Doo <ldoo@sfu.ca>
2016-03-14 22:36:16 -0700

    iter2 to heroku

--- db/schema.rb
+++ db/schema.rb
@@ -1,56 +1,3 @@
-<<<<<<< HEAD
-# encoding: UTF-8
-# This file is auto-generated from the current state of the database. Instead
-# of editing this file, please use the migrations feature of Active Record to
-# incrementally modify your database, and then regenerate this schema definition.
-#
-# Note that this schema.rb definition is the authoritative source for your
-# database schema. If you need to create the application database on another
-# system, you should be using db:schema:load, not running all the migrations
-# from scratch. The latter is a flawed and unsustainable approach (the more migrations
-# you'll amass, the slower it'll run and the greater likelihood for issues).
-#
-# It's strongly recommended that you check this file into your version control system.
-
-ActiveRecord::Schema.define(version: 20160312042633) do
-
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
-  create_table "users", force: :cascade do |t|
-    t.string   "username"
-    t.string   "email"
-    t.string   "street_address"
-    t.string   "city"
-    t.string   "country"
-    t.string   "postal_code"
-    t.boolean  "lol"
-    t.boolean  "dota2"
-    t.boolean  "smite"
-    t.boolean  "hots"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
-    t.float    "longitude"
-    t.float    "latitude"
-    t.string   "password_digest"
-    t.text     "description"
-    t.boolean  "admin",               default: false
-    t.string   "remember_digest"
-    t.string   "activation_digest"
-    t.boolean  "activated",           default: false
-    t.datetime "activated_at"
-    t.string   "state"
-    t.string   "reset_digest"
-    t.datetime "reset_sent_at"
-    t.string   "avatar_file_name"
-    t.string   "avatar_content_type"
-    t.integer  "avatar_file_size"
-    t.datetime "avatar_updated_at"
-    t.string   "twitter_name"
-  end
-
-end
-=======
 # encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
@@ -89,17 +36,16 @@ ActiveRecord::Schema.define(version: 20160312042633) do
     t.boolean  "admin",                           default: false
     t.string   "remember_digest",     limit: 255
     t.string   "activation_digest",   limit: 255
-    t.boolean  "activated",                       default: false
+    t.boolean  "activated",                       default: true
     t.datetime "activated_at"
     t.string   "state",               limit: 255
     t.string   "reset_digest",        limit: 255
     t.datetime "reset_sent_at"
-    t.string   "avatar_file_name",    limit: 255
-    t.string   "avatar_content_type", limit: 255
+    t.string   "twitter_name"
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
     t.integer  "avatar_file_size"
     t.datetime "avatar_updated_at"
-    t.string   "twitter_name",        limit: 255
   end
 
 end
->>>>>>> e96805c7bae55562af76a1994e1e1181a114adee
