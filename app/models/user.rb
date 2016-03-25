accfd44599b88e1d50ee7f9b8fdb85f087c2b12d
alabecki <alabecki@sfu.ca>
2016-03-25 06:00:33 +0000

    add images fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User<ActiveRecord::Base
 
 #PaperClip
-has_attached_file :avatar, :styles => {medium: '200x200'},
+has_attached_file :avatar, :styles => {medium: '200x200', thumb: '100x100'},
 
 :s3_credentials => {
   :bucket =>'group3dev',
