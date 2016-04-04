af5c33c2ae97bff654f506233c9ad293cfb88e76
Lily Doo <ldoo@sfu.ca>
2016-04-03 20:48:07 -0700

    default avatar try 7

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User<ActiveRecord::Base
 
 #PaperClip
 has_attached_file :avatar, :styles => {medium: '200x200', thumb: '100x100'}, 
-:default_url => "http://i.imgur.com/gDhmCKF.png",
+:default_url => "http://i.imgur.com/sI2eQEj.jpg",
 
 :s3_credentials => {
   :bucket =>'group3dev',
