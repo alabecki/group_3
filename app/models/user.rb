84ff77121e122e03fac9f9b34e14240a4ef43f9a
Lily Doo <ldoo@sfu.ca>
2016-04-03 20:21:27 -0700

    default avatar try 4

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User<ActiveRecord::Base
 
 #PaperClip
 has_attached_file :avatar, :styles => {medium: '200x200', thumb: '100x100'}, 
-:default_url => "logo.png",
+:default_url => "default.png",
 
 :s3_credentials => {
   :bucket =>'group3dev',
--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -11,4 +11,5 @@ Rails.application.config.assets.version = '1.0'
 # Rails.application.config.assets.precompile += %w( search.js )
 Rails.application.config.assets.precompile += %w( styleprofile.css )
 
-Rails.application.config.assets.precompile += %w( logo.png )
+#Precompile default logo
+Rails.application.config.assets.precompile += %w( default.png )
