0ba39a2b7b644b697d350ff616399ab6ae9e4883
bwconrad <benwconrad@gmail.com>
2016-04-04 23:59:48 +0000

    profile hiding tests

--- spec/features/profile_view_test_spec.rb
+++ spec/features/profile_view_test_spec.rb
@@ -34,4 +34,33 @@ describe "profile view tests" do
         expect(page).to have_content "State/Province"
         expect(page).to have_content "BC" 
     end
+    
+    it "hides description when empty" do 
+        click_link('PROFILE', match: :first)
+        expect(page).to_not have_content "About me"
+        
+        # Add a description
+        click_link('SETTINGS')
+        fill_in 'Description', :with => 'Hello'
+        click_button 'Save changes'
+        expect(page).to have_content "About me"
+        expect(page).to have_content "Hello" 
+    end
+    
+    it "hides games when empty" do 
+        click_link('SETTINGS')
+        uncheck('Lol')
+        click_button 'Save changes'
+        
+        expect(page).to_not have_content "Games Played"
+        
+        # Add games
+        click_link('SETTINGS')
+        check('Dota2')
+        click_button 'Save changes'
+        expect(page).to have_content "Games Played"
+        have_css("img[src*='http://i.imgur.com/8omtByM.png']")
+ 
+    end
+    
 end
