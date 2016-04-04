f6e772f63dbd616046c38e1ef1082f2fd9a48464
Lily Doo <ldoo@sfu.ca>
2016-04-03 19:41:51 -0700

    testing auto avatar

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User<ActiveRecord::Base
 
 #PaperClip
-has_attached_file :avatar, :styles => {medium: '200x200', thumb: '100x100'},
+has_attached_file :avatar, :styles => {medium: '200x200', thumb: '100x100'}, :default_url => ":medium/ballon.jpg",
 
 :s3_credentials => {
   :bucket =>'group3dev',
